1a49ad5439c2d28189e032b6791342879463a959
laurenbgod <lauren.godwin@outlook.com>
2024-05-14 21:00:29 -0400

    lesson 15 debugged

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-//Creating footer 
+//Creating footer
 const body = document.querySelector("body");
-const newFooter = document.createElement("footer")
+const newFooter = document.createElement("footer");
 body.appendChild(newFooter);
 
 //insert copyright text in footer
@@ -13,74 +13,71 @@ const copyright = document.createElement("p");
 copyright.innerHTML = `<p> Lauren Godwin ${thisYear} </p>`;
 footer.appendChild(copyright);
 
-//Add skills section 
+//Add skills section
 const skills = ["Javascipt", "HTML", "CSS", "GitHub", "Stata"];
 const skillsSection = document.querySelector("#my-skills");
-const skillsList = skillsSection.querySelector("ul")
+const skillsList = skillsSection.querySelector("ul");
 
-for (let i = 0; i <skills.length; i++){
-    const skill = document.createElement("li");
-    skill.innerText = skills[i];
-    skillsList.appendChild(skill);
+for (let i = 0; i < skills.length; i++) {
+  const skill = document.createElement("li");
+  skill.innerText = skills[i];
+  skillsList.appendChild(skill);
 }
 //get form element
-const messageForm = document.querySelector("[name='leave_message']")
+const messageForm = document.querySelector("[name='leave_message']");
 
 //event listener
 messageForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const usersName = event.target.usersName.value;
-    const usersEmail = event.target.usersEmail.value;
-    const usersMessage = event.target.usersMessage.value;
-    console.log(usersName, usersEmail, usersMessage);
+  event.preventDefault();
+  const usersName = event.target.usersName.value;
+  const usersEmail = event.target.usersEmail.value;
+  const usersMessage = event.target.usersMessage.value;
+  console.log(usersName, usersEmail, usersMessage);
 
-//displagy messages in list
-const messageSection = document.querySelector("#messages");
-const messageList = messageSection.querySelector("ul");
-const newMessage = document.createElement("li");
-newMessage.innerHTML = `<a href=mailto:${usersEmail}>${usersName}</a><br>
+  //displagy messages in list
+  const messageSection = document.querySelector("#messages");
+  const messageList = messageSection.querySelector("ul");
+  const newMessage = document.createElement("li");
+  newMessage.innerHTML = `<a href=mailto:${usersEmail}>${usersName}</a><br>
 <span>${usersMessage}</span>`;
 
-//button stuff & event listener
-const removeButton = document.createElement("button");
-removeButton.innerText = "remove";
-removeButton.type = "button";
+  //button stuff & event listener
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "remove";
+  removeButton.type = "button";
 
-removeButton.addEventListener("click", function(event){
+  removeButton.addEventListener("click", function (event) {
     const entry = event.target.parentNode;
     entry.remove();
-});
-
-newMessage.appendChild(removeButton);
-messageList.appendChild(newMessage);
-
-    messageForm.reset();
-})
-
-const projectsSection = document.querySelector("#projects");
-const projectsList = projectsSection.querySelector("ul");
-const repositories = json.parse;
-
-fetch('https://api.github.com/users/laurenbgod/repos')
-    .then((res) => {
-        if (!res.ok) {
-        throw new Error("Something went wrong 😢");
-        }
-        return res.json();
-    })
- 
-    .then((repositories) => {
-        console.log(repositories);
-        for (let i = 0; i < repositories.length; i++) {
-            const project = document.createElement("li");
-            project.innerText = repositories[i].name;
-            projectsList.appendChild(project);
-          }
-        })
-    .catch((error) => {
-        const errorElement = document.createElement("p");
-        errorElement.innerText = error.message;
-        projectsSection.appendChild(errorElement);
   });
 
-    
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+
+  messageForm.reset();
+});
+
+const projectsSection = document.querySelector("#recent-projects");
+const projectsList = projectsSection.querySelector("#projectList");
+
+fetch("https://api.github.com/users/laurenbgod/repos")
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("Something went wrong 😢");
+    }
+    return res.json();
+  })
+
+  .then((repositories) => {
+    console.log(repositories);
+    for (let i = 0; i < repositories.length; i++) {
+      const project = document.createElement("li");
+      project.innerText = repositories[i].name;
+      projectsList.appendChild(project);
+    }
+  })
+  .catch((error) => {
+    const errorElement = document.createElement("p");
+    errorElement.innerText = error.message;
+    projectsSection.appendChild(errorElement);
+  });
